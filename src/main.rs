75254c63a8ddd1c25fdc54e63585b4f122fae611
HubCodes <krlrhkstk12@gmail.com>
2020-08-22 23:03:09 +0900

    quadratic bezier curve~

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use minifb::{MouseMode, Window, WindowOptions, ScaleMode, Scale};
-use raqote::{DrawTarget, SolidSource, Source, DrawOptions, PathBuilder, Point, Transform, StrokeStyle, Color};
+use raqote::{DrawTarget, SolidSource, Source, DrawOptions, PathBuilder, Point, Transform, StrokeStyle, Color, Gradient, GradientStop, Spread};
 use font_kit::family_name::FamilyName;
 use font_kit::properties::Properties;
 use font_kit::source::SystemSource;
@@ -24,12 +24,35 @@ fn main() {
         let mut pb = PathBuilder::new();
         if let Some(pos) = window.get_mouse_pos(MouseMode::Clamp) {
             pb.move_to(pos.0, pos.1);
-            pb.line_to(pos.0 + 100., pos.1);
-            pb.line_to(pos.0 + 100., pos.1 + 100.);
-            pb.line_to(pos.0, pos.1 + 100.);
+            // pb.line_to(pos.0 + 100., pos.1);
+            pb.quad_to(pos.0 + 100., pos.1 - 50., pos.0 + 100., pos.1 + 100.);
+            // pb.line_to(pos.0 + 100., pos.1 + 100.);
+            // pb.line_to(pos.0, pos.1 + 100.);
             // pb.rect(pos.0, pos.1, 100., 100.);
             let path = pb.finish();
-            dt.fill(&path, &Source::Solid(SolidSource::from(Color::new(255, 0, 0, 0))), &DrawOptions::new());
+            let gradient = Source::new_radial_gradient(
+                Gradient {
+                    stops: vec![
+                        GradientStop {
+                            position: 0.,
+                            color: Color::new(0xff, 0, 0xff, 0),
+                        },
+                        GradientStop {
+                            position: 0.8,
+                            color: Color::new(0xff, 0xff, 0xff, 0xff),
+                        },
+                        GradientStop {
+                            position: 1.0,
+                            color: Color::new(0xff, 0xff, 0, 0xff),
+                        },
+                    ],
+                },
+                Point::new(150., 150.),
+                128.,
+                Spread::Pad,
+            );
+            // let solid = Source::Solid(SolidSource::from(Color::new(255, 0, 0, 0)));
+            dt.fill(&path, &gradient, &DrawOptions::new());
 
             let pos_string = format!("{:?}", pos);
             dt.draw_text(
@@ -37,7 +60,7 @@ fn main() {
                 36.,
                 &pos_string,
                 Point::new(50., 50.),
-                &Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0, 0, 0)),
+                &Source::Solid(SolidSource::from_unpremultiplied_argb(0xff, 0xff, 0, 0)),
                 &DrawOptions::new());
             window.update_with_buffer(dt.get_data(), size.0, size.1).unwrap();
         }
